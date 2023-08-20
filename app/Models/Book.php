cd6f0f5ff4a24f164eeaa8d5fa65e57828cf6f5c
remi <remi.nkjm@gmail.com>
2023-08-20 18:15:45 +0900

    Many To Many

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title',
+    ];
+        
+    public function users()
+    {
+    return $this->belongsToMany(User::class);
+    }
 }
